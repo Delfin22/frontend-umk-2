90dbc348648d4102cad3ae22814dc261fb605407
Delfin22 <czarek.arszynski@gmail.com>
2024-11-21 08:44:55 +0100

    Zad1 commit

---
+++ zad1/functions.js
@@ -0,0 +1,11 @@
+function isOdd(n) {
+  return n % 2 != 0 ? true : false;
+}
+function isEven(n) {
+  return n % 2 == 0 ? true : false;
+}
+
+console.log(isOdd(2));
+console.log(isOdd(3));
+console.log(isEven(2));
+console.log(isEven(3));
